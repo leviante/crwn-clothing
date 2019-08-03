6f0438df885177ef2c434ce2696c7e9526c430e1
Berk Açıkgöz <acikgozberk96@gmail.com>
2019-08-03 18:18:52 +0300

    updating our application with rselect, replaced cart selectors with memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,30 @@
+//we do this in order to use reselect properly
+import { createSelector } from "reselect";
+
+//two types of selectors
+/* 
+1- Input selector: doesnt use create selector
+2- output selector: use input and create selector
+*/
+
+//1. input
+const SelectCart = state => state.cart;
+//a function that gets the whole state and returns a slice of it, one layer deep usually
+
+export const selectCartItems = createSelector(
+  //takes 2 arg, first is a collection
+  [SelectCart],
+  //a func that return the value we want out of this selector
+  cart => cart.cartItems
+);
+//selectCardItems is a memoi selector now
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
