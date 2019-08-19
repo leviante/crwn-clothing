a8857eeff6d6e6f7a19799c3b2456b2ecd5a396b
Berk <acikgozberk96@gmail.com>
2019-08-19 15:05:51 +0300

    handling stripe error(1)

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -28,7 +28,6 @@ const StripeCheckoutButton = ({price}) => {
         }).then(response => {
             alert("Payment successful");
         }).catch(error => {
-            console.log("Payment error: ", JSON.parse(error));
             alert("There was an issue with your payment. Please make sure you use the provided credit card");
         });
     }
